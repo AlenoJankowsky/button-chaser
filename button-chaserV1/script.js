6656e8de0f5260c556f851968afb3aecf1132533
Aleno <aleno.jankowsky@5minds.de>
2022-09-22 16:06:44 +0200

    Add script to button-chaserV1

--- button-chaserV1/script.js
+++ button-chaserV1/script.js
@@ -1,2 +1,15 @@
-let buttonToBeMoved = document.getElementById("button-to-be-chased");
+document.addEventListener('DOMContentLoaded', function() {
+  let buttonToBeMoved = document.querySelector('#button-div')
+  let buttonCoordinates = buttonToBeMoved.getBoundingClientRect();
+  let buttonXCoordinates = buttonCoordinates.left;
+  let buttonYCoordinates = buttonCoordinates.top;
 
+  function cursorIsNearButton(event) {
+      let x = event.clientX;
+      let y = event.clientY;
+      if (buttonXCoordinates - x < 5 && y - buttonYCoordinates < 5) {
+          buttonXCoordinates = buttonXCoordinates + 30;
+          buttonYCoordinates = buttonYCoordinates + 30;
+      }
+  }
+});
