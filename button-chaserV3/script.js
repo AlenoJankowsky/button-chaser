6468bb2bce9300a71f816c339a5a2ce92915d151
Aleno <aleno.jankowsky@5minds.de>
2022-09-26 16:05:32 +0200

    Add logic for button

---
+++ button-chaserV3/script.js
@@ -0,0 +1,53 @@
+function cursorIsNearButton(event) {
+  let buttonCoordinates = document.querySelector('.body__container__button-class').getBoundingClientRect();
+  let buttonXCoordinates = buttonCoordinates.left;
+  let buttonYCoordinates = buttonCoordinates.top;
+  let buttonLengthYCoordinates = buttonCoordinates.top + buttonCoordinates.height;
+  let buttonWidthXCoordinates = buttonCoordinates.left + buttonCoordinates.width;
+  let mouseXPosition = event.clientX;
+  let mouseYPosition = event.clientY;
+  let cursorIsNearButton = mouseXPosition > (buttonXCoordinates - 40) && mouseXPosition < (buttonWidthXCoordinates + 40) &&
+                           mouseYPosition > (buttonYCoordinates - 40) && mouseYPosition < (buttonLengthYCoordinates + 40);
+
+  return cursorIsNearButton;
+}
+
+function readMousePosition(event) {
+  let mouseXPosition = event.clientX;
+  let mouseYPosition = event.clientY;
+  let mouseCoordinates = [mouseXPosition, mouseYPosition];
+  
+  return mouseCoordinates;
+}
+
+let referenceArray = [];
+function getMousePositionsInArray(event) {
+  for (let iterationCounter = 0; iterationCounter <= 1; iterationCounter++) {
+    let currentCoords = readMousePosition(event);
+    referenceArray[iterationCounter] = currentCoords;
+    setTimeout(500);
+  }
+
+  return referenceArray;
+}
+
+function moveButton(event) {
+  if (cursorIsNearButton(event)) {
+    let buttonCoordinates = document.querySelector('.body__container__button-class').getBoundingClientRect();
+    let buttonXCoordinates = buttonCoordinates.left;
+    let buttonYCoordinates = buttonCoordinates.top;
+    let directionOfX = referenceArray[1][0] - referenceArray[0][0];
+    let directionOfY = referenceArray[1][1]- referenceArray[0][1];
+    document.getElementById("demo").innerHTML = referenceArray;
+    let computedXCoordinates = buttonXCoordinates + directionOfX;
+    let computedYCoordinates = buttonYCoordinates + directionOfY;
+    let coordinateXNumberToString = computedXCoordinates.toString();
+    let coordinateYNumberToString = computedYCoordinates.toString();
+    let joinedStringForX = coordinateXNumberToString + "px";
+    let joinedStringForY = coordinateYNumberToString + "px";
+    document.getElementById('button-div').style.left = joinedStringForX;
+    document.getElementById('button-div').style.top = joinedStringForY;
+
+    return;
+  }
+}
